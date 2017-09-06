e3ff0ed0c94fb2529574585a222106a4f6d00987
methesoul <diwas.bhattarai@web.de>
2017-09-06 16:31:40 +0200

    Rest call with basic Authentification

--- src/main/java/org/commons/http/HTTPManager.java
+++ src/main/java/org/commons/http/HTTPManager.java
@@ -5,9 +5,21 @@ import java.io.IOException;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -18,77 +30,31 @@ import org.commons.models.WebRequest;
 public class HTTPManager {
 	private HttpHost httpHost = null; 
 	private CloseableHttpClient httpClient = null;
-	
-	public HTTPManager() {
-		httpClient  = HttpClients.createDefault();
-	}
-
-	
-	private void setHost(String host, int port, String connectionType){
-		httpHost = new HttpHost(host,port,connectionType); 
-	}
-	
-	public WebRequest getResponseFromURL(String url) 
-	 { 
-		WebRequest webRequest = new WebRequest();
-	  try
-	  {
-		  HttpGet httpget = new HttpGet(url);
-		  CloseableHttpResponse httpResponse = httpClient.execute(httpget);  //HttpRequest
-		  HttpEntity entity = httpResponse.getEntity();
-		  webRequest.setContent(EntityUtils.toString(entity, "UTF-8"));
-		  webRequest.setHeader(httpResponse.toString());
-	  }
-	  catch (IOException e)
-	  { 
-	   e.printStackTrace(); 
-	  } 
-	  return webRequest;
-	 }
-
-	public WebRequest webResponse(String host, String target, int port, String httpsProtocol, String callMethod) throws Exception{
-		setHost(host, port, httpsProtocol);
-		if(callMethod == "GET"){
-			return getGETResponse(target);
-		}else if(callMethod == "POST"){
-			return getPOSTResponse(target);
-		} else{
-			throw new Exception("No Call Method Found.");
-		}
-	}
+	private CredentialsProvider credsProvider = null;
+	private  AuthCache authCache = null;
 	
 	
-	public WebRequest webResponse(String url, String callMethod) throws Exception{
-		if(callMethod == "GET"){
-			return getResponseFromURL(url);
-		}else if(callMethod == "POST"){
-			return getResponseFromURL(url);
-		}else{
-			throw new Exception("No Call Method Found");
-		}
-	}
+	private void initialize(){
 	
-	
-	private WebRequest getPOSTResponse(String target) {
-		  HttpPost httpPost = new HttpPost(target);
-		  return getResponse(httpPost);
+			httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
 		
+		 
 	}
-
 	
-	public WebRequest getGETResponse(String target){
-		  HttpGet httpget = new HttpGet(target);
-		  return getResponse(httpget);
-	}
-
-
-	
- 	private WebRequest getResponse(HttpRequest httpRequest){
+	private WebRequest getResponse(HttpRequest httpRequest){
  		WebRequest webRequest = null;
 		HttpEntity entity = null;
+		CloseableHttpResponse httpResponse = null;
+		HttpClientContext context = HttpClientContext.create();
+		context.setAuthCache(authCache);
 		try
 		{
-			CloseableHttpResponse httpResponse = httpClient.execute(httpHost, httpRequest); 
+			if(httpHost == null){
+				httpResponse = httpClient.execute((HttpUriRequest) httpRequest); 
+			}else{
+				httpResponse = httpClient.execute(httpHost, httpRequest, context); 
+			}
+			
 			entity = httpResponse.getEntity();
 			webRequest = new WebRequest();
 			webRequest.setContent(EntityUtils.toString(entity));
@@ -97,11 +63,49 @@ public class HTTPManager {
 		  	catch (IOException e) 
 		 { 
 		  		e.printStackTrace(); 
+		 }finally{
+			 try {
+				 if(httpResponse!=null){
+				httpResponse.close();
+				 }
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		 }
 		 
 		return webRequest;
 	}
 	
+	public void setHost(String host, int port, String scheme){
+		httpHost = new HttpHost(host,port,scheme); 
+	}
+
+	public void setAuthentification(String username, final String password){
+	
+		credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(new AuthScope(httpHost), new UsernamePasswordCredentials(username, password));
+        // Create AuthCache instance
+        authCache = new BasicAuthCache();
+        // Generate BASIC scheme object and add it to the local auth cache
+        BasicScheme basicAuth = new BasicScheme();
+        authCache.put(httpHost, basicAuth);
+        
+	}
+			
+	public WebRequest getPOSTResponse(String target) {
+		initialize();
+		HttpPost httpPost = new HttpPost(target);
+		return getResponse(httpPost);
+		
+	}
+	
+	public WebRequest getGETResponse(String target){
+		initialize();
+        HttpGet httpGet = new HttpGet(target);
+        return  getResponse(httpGet);
+	}
+ 	
 
 	public void dispose(){
 		try {
@@ -113,4 +117,5 @@ public class HTTPManager {
 		
 	}
 	
+		
 }
