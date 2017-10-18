f63e6ac4a9d6eb8d5cdb7d6c22180c6c4dee6799
methesoul <support@lifestohack.com>
2017-10-18 23:57:00 +0200

    Properties file updated

--- src/main/java/org/commons/constants/DIConstants.java
+++ src/main/java/org/commons/constants/DIConstants.java
@@ -4,5 +4,10 @@ public class DIConstants {
 	public static final String PROPERTIES_PATH = "src/main/resources/Properties";
 	public static final String PROPERTIES_FILE_PATH = "functional.properties";
 	public static final String PROPERTIES_VALUE_FILE_PATH = ".values";
+	public static final String TECHNICAL_PROPERTIES = "technical.properties";
+	public static final String BROWSER_PROPERTY = "browser";
+	public static final String BROWSERPATH_PROPERTY = "browserPath";
+	public static final String URL_PROPERTY = "url";
+	
 
 }
